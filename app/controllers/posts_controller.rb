62c5732923f877d4df30750b7777530c0c7b5728
ch2isk4kos <ch2isk4kos@gmail.com>
2019-06-12 23:17:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		@categories = Category.all
 	end
 
 	def new
@@ -13,18 +15,34 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.create(params[:post])
-		redirect_to post_path(post)
+		@post = Post.new(post_params)
+
+		if @post.valid?
+			@post.save
+			redirect_to post_path(@post)
+		else
+			render :new
+		end
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 		@categories = Category.all
 	end
 
 	def update
-		post = Post.find(params[:id])
-		post.update(params.require(:post))
-		redirect_to post_path(post)
+		if @post.update(post_params)
+			redirect_to post_path(@post)
+		else
+			render :edit
+		end
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
 	end
 end
